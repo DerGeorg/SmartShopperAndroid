36bc515c4b30b5d1bb602e1c52a9de606a8f6f26
Georg Reisinger <43533139+GeorgReisinger@users.noreply.github.com>
2019-01-26 20:00:38 +0100

    ActionBar, Menu, Logout
    ActionBar hinzugefügt
    Menu zu ActionBar hinzugefügt
    Logout jetzt in Actionbar

--- app/src/main/java/at/smartshopper/smartshopper/Dash.java
+++ app/src/main/java/at/smartshopper/smartshopper/Dash.java
@@ -1,13 +1,20 @@
 package at.smartshopper.smartshopper;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
@@ -29,10 +36,7 @@ public class Dash extends AppCompatActivity {
     private Database db = new Database();
     private SwipeRefreshLayout ownswiperefresh;
 
-    private void goLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
+
 
 
     @Override
@@ -41,20 +45,12 @@ public class Dash extends AppCompatActivity {
         setContentView(R.layout.activity_dash);
 
 
+        // Erstellt die Tabs
         tabHoster();
-
-        Button logoutBtn = (Button) findViewById(R.id.logoutBtn);
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                goLogin();
-            }
-        });
 
 
         /*
-        Get userinformations
+        Get userinformations and show them
          */
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
@@ -93,10 +89,20 @@ public class Dash extends AppCompatActivity {
     }
 
     /**
+     * Logt den User aus und geht zur Login Activity
+     */
+    private void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
      * Refreshed die eigene shoppinglist und veranlasst das das refreshen beendet wird
+     *
      * @param uid Von dem benutzer von welchem die Shoppinglists angezeigt werden sollen
      */
-    private void refreshOwnShoppinglist(String uid){
+    private void refreshOwnShoppinglist(String uid) {
         try {
             showOwnShoppingList(uid);
         } catch (JSONException e) {
@@ -152,31 +158,30 @@ public class Dash extends AppCompatActivity {
         host.addTab(spec);
     }
 
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.dash_menu, menu);
         return true;
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.logoutBtn:
+                logout();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
-}
-    */
 
 
 }
