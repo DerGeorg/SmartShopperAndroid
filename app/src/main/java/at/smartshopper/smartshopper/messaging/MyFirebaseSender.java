4eda7a13b2b7b8b211c5d61ed37ae3ab6cae2470
Georg Reisinger <43533139+GeorgReisinger@users.noreply.github.com>
2019-02-24 01:38:53 +0100

    /push/:msgtoken funktioniert

--- app/src/main/java/at/smartshopper/smartshopper/messaging/MyFirebaseSender.java
+++ app/src/main/java/at/smartshopper/smartshopper/messaging/MyFirebaseSender.java
@@ -1,13 +1,23 @@
 package at.smartshopper.smartshopper.messaging;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 import at.smartshopper.smartshopper.R;
 import at.smartshopper.smartshopper.shoppinglist.Member;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
 
 public class MyFirebaseSender {
 
@@ -32,11 +42,24 @@ public class MyFirebaseSender {
         FirebaseMessaging firebaseMessaging = FirebaseMessaging.getInstance();
         for (int i = 0; messageIds.size() > i; i++) {
 
-            firebaseMessaging.send(new RemoteMessage.Builder(R.string.firebase_sender_id + "@fcm.googleapis.com/fcm/send")
-                    .setMessageId(messageIds.get(i))
-                    .addData("my_message", message)
-                    .addData("my_action", action)
-                    .build());
+            try {
+                firebaseMessaging.send(new RemoteMessage.Builder(R.string.firebase_sender_id + "@fcm.googleapis.com/fcm/")
+                        .setMessageId(messageIds.get(i))
+                        .addData("my_message", message)
+                        .addData("LoginActivity", action)
+                        .build());
+
+                // Create a new HttpClient and Post Header
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("https://www.smartshopper.cf/push/" + messageIds.get(i));
+
+                    // Execute HTTP Post Request
+                    HttpResponse response = httpclient.execute(httppost);
+                    Log.d(R.string.StringTag + "", "Response Push Post" + response);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
